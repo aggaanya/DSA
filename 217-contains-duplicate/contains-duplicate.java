b30c84e36a25be2a611e2c6dd2b914b5f68c2b0f
Aanya Aggarwal <aggaanya15@gmail.com>
2025-07-15 00:00:33 +0530

    Time: 21 ms (20.24%) | Memory: 58.2 MB (62.26%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,12 @@
+class Solution {
+    public static boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+
+        for (int index = 0; index < nums.length - 1; index++) {
+            if (nums[index] == nums[index + 1]){
+                return true;
+            }
+        }
+        return false;
+    }
+}
