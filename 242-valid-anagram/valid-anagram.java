bc7b0156e33d4501e984e594f1ff02d67e8407b9
Aanya Aggarwal <aggaanya15@gmail.com>
2025-07-19 15:37:38 +0530

    Time: 4 ms (77.29%) | Memory: 44.4 MB (56.15%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,31 +1,9 @@
 class Solution {
-    public static boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) return false;
-
-        Map<Character, Integer> countMap = new HashMap<>();
-        char [] str1 = s.toCharArray();
-        char [] str2 = t.toCharArray();
-
-        for (char ch: str1){
-            //counting the frequency of each character
-            if (countMap.containsKey(ch)){
-                //if the character is already present in the hashmap increase the frequency
-                countMap.put(ch, countMap.get(ch) + 1);
-            }else{
-                countMap.put(ch, 1);
-            }
-        }
-        Map<Character, Integer> countMap2 = new HashMap<>();
-        //now i have compare the frequency from the second string
-        for (char ch: str2){
-            //counting the frequency of each character
-            if (countMap2.containsKey(ch)){
-                //if the character is already present in the hashmap increase the frequency
-                countMap2.put(ch, countMap2.get(ch) + 1);
-            }else{
-                countMap2.put(ch, 1);
-            }
-        }
-        return countMap.equals(countMap2);
+    public static boolean isAnagram(String s, String t){
+        char [] ch1 = s.toCharArray();
+        char [] ch2 = t.toCharArray();
+        Arrays.sort(ch1);
+        Arrays.sort(ch2);
+        return Arrays.equals(ch1, ch2);
     }
 }
