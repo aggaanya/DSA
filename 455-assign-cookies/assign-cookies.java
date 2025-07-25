3bf814cd1a304ee22149e06e9d31baaafb0ae7c2
Aanya Aggarwal <aggaanya15@gmail.com>
2025-07-25 18:41:39 +0530

    Time: 9 ms (98.87%) | Memory: 48.4 MB (84.74%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.java
@@ -0,0 +1,17 @@
+class Solution {
+    public static int findContentChildren(int[] g, int[] s) {
+        int i = 0;
+        int j = 0;
+        int count = 0;
+        Arrays.sort(g);
+        Arrays.sort(s);
+        while (i < g.length && j < s.length){
+            if (g[i] <= s[j]){
+                count++;
+                i++;
+            }
+            j++;
+        }
+        return count;
+    }
+}
