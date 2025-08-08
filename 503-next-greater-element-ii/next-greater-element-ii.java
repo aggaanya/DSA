0831e0931a96b91dfee3eb5d846b9948af0c1970
Aanya Aggarwal <aggaanya15@gmail.com>
2025-08-08 15:15:27 +0530

    Time: 15 ms (71.14%) | Memory: 46.4 MB (49.03%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -1,16 +1,20 @@
 class Solution {
     public static int[] nextGreaterElements(int[] nums) {
-        //this is the circular array problem
-        int [] ans = new int[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            ans[i] = -1;
-            for (int j = i + 1; j <= nums.length - 1 + i; j++) {
-                int index = j % nums.length;
-                if (nums[index] > nums[i]) {
-                    ans[i] = nums[index];
-                    break;
-                }
+        int n = nums.length;
+        int[] ans = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        // Traverse the array twice to simulate circular behavior
+        for (int i = 2 * n - 1; i >= 0; i--) {
+            int index = i % n;
+            // Maintain decreasing stack
+            while (!stack.isEmpty() && stack.peek() <= nums[index]) {
+                stack.pop();
             }
+            // Only fill answer during the first pass
+            if (i < n) {
+                ans[index] = stack.isEmpty() ? -1 : stack.peek();
+            }
+            stack.push(nums[index]);
         }
         return ans;
     }
