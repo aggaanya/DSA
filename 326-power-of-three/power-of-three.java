3bc51dc6dbf3da3da388e8da76f33c559257ebb4
Aanya Aggarwal <aggaanya15@gmail.com>
2025-09-18 13:24:03 +0530

    Time: 8 ms (81.54%) | Memory: 43.9 MB (94.92%) - LeetSync

--- 326-power-of-three/power-of-three.java
+++ 326-power-of-three/power-of-three.java
@@ -1,15 +1,6 @@
 class Solution {
     public static boolean isPowerOfThree(int n) {
-        if(n == 1){
-            return true;
-        }
-        if(n <= 0){
-            return false;
-        }
-        if(n % 3 != 0){
-            //this means that this number is not divisible by 3
-            return false;
-        }
-        return isPowerOfThree(n / 3);
+        double ans = Math.log10(n) / Math.log10(3);
+        return ans == (int)ans;
     }
 }
