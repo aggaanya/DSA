40c40480d65ebfbb187d0cbd4aa39024f1d18f4a
Aanya Aggarwal <aggaanya15@gmail.com>
2025-11-01 19:06:43 +0530

    Time: 22 ms (26.60%) | Memory: 178.3 MB (5.52%) - LeetSync

---
+++ 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode modifiedList(int[] nums, ListNode head) {
+        HashSet<Integer> set = new HashSet<>();
+        //adding the values into the set
+
+        for(int num: nums){
+            set.add(num);
+        }
+
+        ListNode prev = null;
+        ListNode curr = head; 
+
+        while(curr != null){
+            if(set.contains(curr.val)){
+                //this is the first case, when the head of the liskedlist is itself present in the array
+                if(curr.val == head.val){
+                    head = head.next;
+                }
+                else{
+                    prev.next = curr.next;
+                }
+            }
+            else{
+                //this is the condition whem the node is not present in the nums array
+                prev = curr;
+            }
+            curr = curr.next;
+        }
+        return head;
+    }
+}
