809dc55c680e1773acc9bcd95c16c88c719ec8f9
Aanya Aggarwal <aggaanya15@gmail.com>
2025-11-12 20:14:10 +0530

    Time: 6 ms (41.22%) | Memory: 55.3 MB (9.61%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,25 +1,9 @@
 class Solution {
-    public static int majorityElement(int [] arr){
-        HashMap<Integer, Integer> set = new HashMap<>();
-
-        if(arr.length == 0){
-            return -1;
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        if(nums.length == 1){
+            return nums[0];
         }
-        for (int num: arr){
-            if (set.containsKey(num)){
-                //ths is the condition when the element is already existing in the hashmap
-                set.put(num, set.get(num) + 1);
-            }else{
-                //if the element does not exist in hashmap
-                set.put(num, 1);
-            }
-        }
-        //set.keySet()--> this will return the entire key set
-        for(int key: set.keySet()){
-            if (set.get(key) > arr.length / 2){
-                return key;
-            }
-        }
-        return -1;
+        return nums[nums.length/2];
     }
 }
