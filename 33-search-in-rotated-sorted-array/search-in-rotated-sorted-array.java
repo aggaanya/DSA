d959d8fde77768e9ed59d444dc8d31a0fa978462
Aanya Aggarwal <aggaanya15@gmail.com>
2025-11-27 14:34:52 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (33.77%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,29 +1,45 @@
 class Solution {
-    public static int search(int[] nums, int target) {
+    public int search(int[] nums, int target) {
         int start = 0;
         int end = nums.length - 1;
-        while (start <= end){
-            int middle = start + (end - start) / 2;
-            if (nums[middle] == target){
-                return middle;
-            }
-            //identify the sorted part in the arary
-            if (nums[middle] >= nums[start]){
-                //left half is sorted
-                if (nums[start] <= target && target <= nums[middle]){
-                    end = middle - 1;
-                }else{
-                    start = middle + 1;
-                }
-            }
-            else {
-                if (nums[middle] <= target && target <= nums[end]){
-                    start = middle + 1;
-                }else{
-                    end = middle -1;
-                }
+        int index = helper(nums);
+        int ans = BinarySearch(nums, start, target, index - 1);
+        if(ans == -1){
+            return BinarySearch(nums, index, target, end);
+        }
+        return ans;
+    }
+    //this function will return the index of the target element
+    public int BinarySearch(int [] arr, int start, int target, int end){
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+            if(arr[mid] == target){
+                return mid;
+            }else if(arr[mid] < target){
+                start = mid + 1;
+            }else if(arr[mid] > target){
+                end = mid - 1;
             }
         }
         return -1;
+    }
+    //this function will return the index of the minimum element in the array 
+    public int helper(int [] arr){
+        int start = 0;
+        int end = arr.length - 1;
+        
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+            int prev = (mid + arr.length - 1) % arr.length;
+            int next = (mid + 1) % arr.length;
+            if(arr[mid] <= arr[next] && arr[mid] <= arr[prev]){
+                return mid;
+            }else if(arr[mid] >= arr[end]){
+                start = mid + 1;
+                }else{
+                     end = mid - 1;
+        }
+    }
+        return 0;
     }
 }
