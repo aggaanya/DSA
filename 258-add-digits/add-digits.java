b54d40106aa0b98ac0f09dedd7829bf720c35bca
Aanya Aggarwal <aggaanya15@gmail.com>
2026-01-06 06:31:10 +0530

    Time: 1 ms (97.51%) | Memory: 42.8 MB (24.44%) - LeetSync

--- 258-add-digits/add-digits.java
+++ 258-add-digits/add-digits.java
@@ -1,14 +1,11 @@
 class Solution {
     public int addDigits(int num) {
-        //if the num is of the single digit, return the number itself
-        if(num <= 9){
-            return num;
+        if(num == 0){
+            return 0;
         }
-        else if(num % 9 == 0){
+        if(num % 9 == 0){
             return 9;
         }
-        else{
-            return num % 9;
-        }
+        return num % 9;
     }
 }
