6174b41facddb4f179f45d4155acb811f3e90155
Aanya Aggarwal <aggaanya15@gmail.com>
2026-01-17 12:09:25 +0530

    Time: 0 ms (100.00%) | Memory: 48.2 MB (37.22%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,22 +1,47 @@
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
-        int firstOcc = -1;
-        for(int i = 0; i < nums.length; i++){
-            if(target == nums[i]){
-                firstOcc = i;
-                break;
+    public static int[] searchRange(int[] nums, int target) {
+        int [] ans = {-1, -1};
+        int start = 0, end = nums.length - 1;
+        int mid = start + (end - start) / 2;
+        int first = searchLeft(nums, start, end, target);
+        int last = searchRight(nums, start, end, target);
+        if(first != -1){
+            ans[0] = first;
+        }
+        if(last != -1){
+            ans[1] = last;
+        }
+        return ans;
+    }
+    public static int searchLeft(int [] nums, int start, int end, int target){
+        int res = -1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if(nums[mid] == target){
+                res = mid;
+                end = mid - 1;
+            }else if(nums[mid] < target){
+                start = mid + 1;
+            }else{
+                end = mid - 1;
             }
         }
-        int lastOcc = -1;
-        for(int i = nums.length - 1; i >= 0; i--){
-            if(target == nums[i]){
-                lastOcc = i;
-                break;
+        return res;
+    }
+
+    public static int searchRight(int [] nums, int start, int end, int target){
+        int res = -1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if(nums[mid] == target){
+                res = mid;
+                start = mid + 1;
+            }else if(nums[mid] < target){
+                start = mid + 1;
+            }else{
+                end = mid - 1;
             }
         }
-        if(firstOcc != -1 && lastOcc != -1){
-            return new int[]{firstOcc, lastOcc};
-        }
-        return new int[]{firstOcc, lastOcc};
+        return res;
     }
 }
